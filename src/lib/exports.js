6d891fbd403c32efc540f70775e26f9fc72b295a
marinamassaneiro <marinamassaneiro@gmail.com>
2022-09-30 09:31:21 -0300

    firestore fazendo banco de dados, mas nao pegando dado

--- src/lib/exports.js
+++ src/lib/exports.js
@@ -1,4 +1,5 @@
 export { initializeApp } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js';//eslint-disable-line
+
 export {
   getAuth,
   signInWithEmailAndPassword,
@@ -9,3 +10,10 @@ export {
   signOut,
   updateProfile,
 } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';//eslint-disable-line
+
+export {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+} from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js';//eslint-disable-line
