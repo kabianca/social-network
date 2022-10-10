1d06efff92fe7cec2b89101b8563bc4da386c2ee
marinamassaneiro <marinamassaneiro@gmail.com>
2022-10-10 18:46:19 -0300

    Updates

--- pages/timeline/post.js
+++ pages/timeline/post.js
@@ -1,80 +1,96 @@
-import { db } from '../../lib/config.js';
-import { collection} from '../../lib/exports.js';
 import { likeRecipe, deslikeRecipe, deleteRecipe } from '../../lib/firestore.js';
+import editRecipe from './editRecipe.js';
 
-export function printTimeline(recipes, timelinePost, user) {
+export default (recipes, timelinePost, user) => {
   recipes.forEach((doc) => {
     const postContainer = document.createElement('div');
-    postContainer.id ='single-post';
+
     let countLikes = doc.likes.length;
+    postContainer.setAttribute('id', 'single-post');
     
     const templatePost = `
         <h1 id="title">${doc.title}</h1>
-        <p>por ${doc.author}</p>
+        <p id="author-post">por ${doc.author}</p>
         <div class="linha1">
           <p>${doc.time}min</p>
           <p>${doc.difficult}</p>
-          <button class="btn-like like-count"><i class="fa-regular fa-heart"></i>${countLikes}</button>
+          <button class="btn-like like-count"><i class="fa-regular fa-heart"></i>${countLikes} curtidas!</button>
         </div>
         <details>
           <summary>Ver mais</summary>
-          <div>${doc.ingredients}</div>
-          <div>${doc.prepare}</div>
+          <div>
+            <ul></ul>
+          </div>
+          <div id="prepare-mode">${doc.prepare}</div>
           <p id="btn-del"></p>
-  
-      `;
+          <p id="btn-edit"></p>
+          <section id="divModal"></section>
+        `;
 
-      postContainer.innerHTML = templatePost;
+    postContainer.innerHTML = templatePost;
 
-      const delPost = postContainer.querySelector('#btn-del');
-      const displayLikes = postContainer.querySelector('.like-count'); 
-      const likeHeart = postContainer.querySelector('.fa-regular');
-      const likeBtn = postContainer.querySelector('.btn-like');
-      let likeForUser = doc.likes.filter((client) => client === user.uid);
+    const delPost = postContainer.querySelector('#btn-del');
 
-        likeBtn.addEventListener('click', (e) => {
-          e.preventDefault();
-          if (likeForUser.length === 0) {
-            likeRecipe(doc.id, user.uid).then(() => {
-              likeForUser.push(user.uid);
-              countLikes += 1;
-              displayLikes.textContent = `${countLikes}`;
-              likeHeart.classList.toggle('fa-solid fa-heart');//não está funcionando agora
-            });
-          } else {
-            deslikeRecipe(doc.id, user.uid).then(() => {
-              likeForUser = [];
-              countLikes -= 1;
-              displayLikes.textContent = `${countLikes}`;
-              likeHeart.classList.toggle('fa-regular fa-heart');//não está funcionando agora
-            });
-          }
+    const displayLikes = postContainer.querySelector('.like-count');
+    const ingredients = doc.ingredients.split(', ');
+    const likeBtn = postContainer.querySelector('.btn-like');
+    const unorderedList = postContainer.querySelector('ul');
+    let likeForUser = doc.likes.filter((client) => client === user.uid);
+
+    ingredients.forEach((item) => {
+      const list = document.createElement('li');
+      unorderedList.appendChild(list);
+      list.innerHTML += item;
+    });
+
+    if (likeForUser.length !== 0) {
+      likeBtn.innerHTML = `<i class="fa-solid fa-heart"></i>${countLikes} curtidas!`;
+    }
+
+    likeBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      if (likeForUser.length === 0) {
+        likeRecipe(doc.id, user.uid).then(() => {
+          likeForUser.push(user.uid);
+          countLikes += 1;
+          displayLikes.textContent = `${countLikes} curtidas!`;
+          likeBtn.innerHTML = `<i class="fa-solid fa-heart"></i>${countLikes} curtidas!`;
         });
+      } else {
+        deslikeRecipe(doc.id, user.uid).then(() => {
+          likeForUser = [];
+          countLikes -= 1;
+          displayLikes.textContent = `${countLikes} curtidas!`;
+          likeBtn.innerHTML = `<i class="fa-regular fa-heart"></i>${countLikes} curtidas!`;
+        });
+      }
+    });
 
-        // if ()
-        
-        delPost.innerHTML=`<button data-remove="${doc.id}">Apagar</button>`;
+    if (user.uid === doc.userUid) {
+      editPost.innerHTML = `<button id="edit">Editar</button>`;
+      const edit = postContainer.querySelector('#edit');
+      edit.addEventListener('click', (e) => {
+        e.preventDefault();
+        const divModal = postContainer.querySelector('#divModal');
+        divModal.appendChild(editRecipe(doc));
+      });
+    
+    delPost.innerHTML = `<button data-remove="${doc.id}">Apagar</button>`;
 
-        postContainer.addEventListener('click', e => {
-          const removeButtonId = e.target.dataset.remove
-          
-          if (removeButtonId){
-            deleteRecipe(removeButtonId)
-            .then(() => {
-              window.location.reload();
-            })
-            .catch( e => {
-              console.log("Erro")
-            })
-          }
-        })
-
-
-      timelinePost.appendChild(postContainer);
+    postContainer.addEventListener('click', e => {
+      if (e.target.dataset.remove) {
+        if (window.confirm('Tem certeza de que deseja excluir a publicação?')) {
+         deleteRecipe(e.target.dataset.remove)
+          .then(() => {
+            window.location.reload();
+          })
+          .catch(e => {
+            console.log("Erro")
+          });
+        }
+       }
+    })
+  }
+    timelinePost.appendChild(postContainer);
   });
-
 }
-
-
-
-
--- pages/timeline/timeline.js
+++ pages/timeline/timeline.js
@@ -1,7 +1,7 @@
 import { logout, auth } from '../../lib/auth.js';
 import recipe from './recipe.js';
 import { printPostagem } from '../../lib/firestore.js';
-import { printTimeline } from './post.js';
+import post from './post.js';
 
 export default async () => {
   const container = document.createElement('div');
@@ -13,7 +13,7 @@ export default async () => {
             <img id="gif-timeline" src="assets/104313-cooking-chef.gif">
         </figure>
         <div id="text-header">
-          <p> Olá, ${auth.currentUser.displayName}!</p>
+          <p> Olá!</p>
           <h3>O que vamos cozinhar hoje?</h3>
         </div>
     </section>  
@@ -21,44 +21,41 @@ export default async () => {
         <button id="btn-modal">Clique aqui para publicar sua receita!</button>
         <section id="divModal"></section>
         <section id="timeline-post"></section>
-        <nav>
-            <ul>
-              <li><a id="btn-home"><i class="fa-solid fa-house fa-2xl"></i></a></li>
-              <li><a id="btn-logout"><i class="fa-solid fa-right-from-bracket fa-2xl"></i></a></li>
-            </ul>
-        </nav>
     </section>
+    <footer>
+      <nav>
+        <ul>
+          <li><a id="btn-home"><i class="fa-solid fa-house fa-2xl"></i></a></li>
+          <li><a id="btn-logout"><i class="fa-solid fa-right-from-bracket fa-2xl"></i></a></li>
+        </ul>
+      </nav>
+    </footer>
     `;
 
   const timelinePost = container.querySelector('#timeline-post');
-
-  const timeline = printTimeline(await printPostagem(), timelinePost, auth.currentUser);
-  // timelinePost.innerHTML = timeline;
-
-  const divModal = container.querySelector('#divModal');
-  divModal.appendChild(recipe());
   const btnModal = container.querySelector('#btn-modal');
+  const btnLogout = container.querySelector('#btn-logout');
+  const btnHome = container.querySelector('#btn-home');
+  
+  post(await printPostagem(), timelinePost, auth.currentUser);
 
   btnModal.addEventListener('click', (event) => {
     event.preventDefault();
-    modal.style.display = 'block';
+    const divModal = container.querySelector('#divModal');
+    divModal.appendChild(recipe());
   });
 
+  btnHome.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
 
-    const btnLogout = container.querySelector('#btn-logout');
-    const btnHome = container.querySelector('#btn-home');
-
-    btnHome.addEventListener('click', (event) => {
-      event.preventDefault();
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
-
-    btnLogout.addEventListener('click', () => {
-      logout()
-        .then(() => {
-          window.location.hash = '#login';
-        });
-    });
+  btnLogout.addEventListener('click', () => {
+    logout()
+      .then(() => {
+        window.location.hash = '#login';
+      });
+  });
 
   return container;
 };
